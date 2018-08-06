f63b8d35c5a001e76b36451b317b975810addca6
HerveH44 <herve.huneau@gmail.com>
2018-08-06 08:38:14 +0200

    Dev: first attempt at imageHelper

--- gui/src/main/js/components/CardsZone/Card.js
+++ gui/src/main/js/components/CardsZone/Card.js
@@ -1,9 +1,11 @@
 import React from "react"
 import {func, object, string} from "prop-types"
 
-const Card = ({card, title = "", onClick= ()=>{}, classNames = ""}) => (
+const Card = ({card, title = "", onClick= ()=>{}, classNames = "", onMouseEnter, onMouseLeave}) => (
     <div className={classNames}
-          onClick={onClick}>
+          onClick={onClick}
+          onMouseEnter={onMouseEnter}
+          onMouseLeave={onMouseLeave}>
         <img src={`http://gatherer.wizards.com/Handlers/Image.ashx?multiverseid=${card.multiverseid}&type=card`}
              alt={card.name}/>
     </div>
@@ -14,7 +16,9 @@ Card.propTypes = {
     card: object.isRequired,
     onClick: func,
     title: string,
-    classNames: string
+    classNames: string,
+    onMouseEnter: func,
+    onMouseLeave: func
 };
 
 export default Card;
--- gui/src/main/js/components/CardsZone/ZonesAsColumns.js
+++ gui/src/main/js/components/CardsZone/ZonesAsColumns.js
@@ -1,4 +1,4 @@
-import React from "react";
+import React, {Component} from "react";
 import {array, func, object, string} from "prop-types";
 import {connect} from "react-redux"
 import {Spaced} from "../utils";
@@ -6,26 +6,85 @@ import Card from "./Card";
 import {getCardsAsMap, MAIN, SIDE} from "../../reducers/playerState";
 import {onClickZone} from "../../actions/game";
 
-const ZonesAsColumns = ({toCardsMap, onClickZone}) => (
-    [MAIN, SIDE].map((zoneName, i) =>
-        <CardsZone key={i}
-                   zoneName={zoneName}
-                   cardsMap={toCardsMap(zoneName)}
-                   onClickCard={(card) => () => onClickZone({zone: zoneName, card})}/>)
+class ZonesAsColumns extends Component {
+    state = {
+        className: "right",
+        card: undefined
+    };
+
+    static propTypes = {
+        toCardsMap: func.isRequired,
+        onClickZone: func.isRequired
+    };
+
+    onMouseEnterCard = (card) => e => {
+        let {offsetLeft} = e.target;
+        let {clientWidth} = document.documentElement;
+
+        let imgWidth = 240;
+        let colWidth = 180;
+
+        let className = offsetLeft + colWidth > clientWidth - imgWidth
+            ? "left"
+            : "right";
+
+        this.setState({ card, className });
+    };
+
+    onMouseLeaveCard = () => {
+        this.setState({
+            card: undefined
+        });
+    };
+
+    render () {
+        const {toCardsMap, onClickZone} = this.props;
+        const {className, card} = this.state;
+        return (
+            <div>
+                {[MAIN, SIDE].map((zoneName, i) =>
+                    <CardsZone key={i}
+                               zoneName={zoneName}
+                               cardsMap={toCardsMap(zoneName)}
+                               onMouseEnterCard={this.onMouseEnterCard}
+                               onMouseLeaveCard={this.onMouseLeaveCard}
+                               onClickCard={(card) => () => onClickZone({zone: zoneName, card})}/>)}
+                <ImageHelper className={className} onMouseEnter={this.onMouseEnterCard} card={card}/>
+            </div>
+        )
+    }
+}
+
+const ImageHelper = ({onMouseEnter, className, card}) => (
+    card
+        ? card.isDoubleFaced
+        ? <div className={className} id="doubleimg">
+            <img className="card" src={card.url} onMouseEnter={onMouseEnter(card)} />
+            <img className="card" src={card.flippedCardURL} onMouseEnter={onMouseEnter(card)} />
+        </div>
+        : <img className={className}
+               id='img'
+               onMouseEnter={onMouseEnter(card)}
+               src={`http://gatherer.wizards.com/Handlers/Image.ashx?multiverseid=${card.multiverseid}&type=card`}/>
+        : <div />
 );
 
-ZonesAsColumns.propTypes = {
-    toCardsMap: func.isRequired,
-    onClickZone: func.isRequired
+ImageHelper.propTypes = {
+    onMouseEnter: func.isRequired,
+    className: string.isRequired,
+    card: object
 };
 
-const CardsZone = ({zoneName, cardsMap, onClickCard}) => {
+const CardsZone = ({zoneName, cardsMap, onClickCard, onMouseEnterCard, onMouseLeaveCard}) => {
     let sum = 0;
     let cols = [];
 
     for (let key in cardsMap) {
         const cards = cardsMap[key].map((card, i) =>
-            <Card key={i} card={card} onClick={onClickCard(card)}/>
+            <Card key={i} card={card}
+                  onClick={onClickCard(card)}
+                  onMouseEnter={onMouseEnterCard(card)}
+                  onMouseLeave={onMouseLeaveCard}/>
         );
 
         sum += cards.length;
@@ -45,7 +104,9 @@ const CardsZone = ({zoneName, cardsMap, onClickCard}) => {
 CardsZone.propTypes = {
     zoneName: string.isRequired,
     cardsMap: object.isRequired,
-    onClickCard: func.isRequired
+    onClickCard: func.isRequired,
+    onMouseEnterCard: func.isRequired,
+    onMouseLeaveCard: func.isRequired
 };
 
 const CardsColumn = ({ cards, keyTitle }) => (
