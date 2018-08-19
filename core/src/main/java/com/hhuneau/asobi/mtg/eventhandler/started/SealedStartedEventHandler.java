9f5fc5f8d2e3be04d019192013947e4b57dfd759
HerveH44 <herve.huneau@gmail.com>
2018-08-19 22:05:09 +0200

    Fix: adjust sealedevent interestedin

--- core/src/main/java/com/hhuneau/asobi/mtg/eventhandler/started/SealedStartedEventHandler.java
+++ core/src/main/java/com/hhuneau/asobi/mtg/eventhandler/started/SealedStartedEventHandler.java
@@ -1,10 +1,13 @@
 package com.hhuneau.asobi.mtg.eventhandler.started;
 
 import com.hhuneau.asobi.customer.CustomerService;
+import com.hhuneau.asobi.mtg.game.Game;
 import com.hhuneau.asobi.mtg.game.GameService;
 import com.hhuneau.asobi.mtg.player.PlayerService;
 import org.springframework.stereotype.Component;
 import org.springframework.transaction.annotation.Transactional;
+
+import static com.hhuneau.asobi.mtg.game.GameType.SEALED;
 
 
 @Component
@@ -12,5 +15,10 @@ import org.springframework.transaction.annotation.Transactional;
 public class SealedStartedEventHandler extends GameStartedEventHandler {
     protected SealedStartedEventHandler(GameService gameService, CustomerService customerService, PlayerService playerService) {
         super(gameService, customerService, playerService);
+    }
+
+    @Override
+    public boolean isInterested(Game game) {
+        return super.isInterested(game) && game.getGameType().equals(SEALED);
     }
 }
